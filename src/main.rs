74aaf349cadc2192db3806af6be6802b68dde6cb
mk3250d07295y@aol.com <Yoika>
2022-11-12 09:52:01 +0900

    add open url

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
+use webbrowser;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let args_iter = args[1..].iter();
+
+    for url in args_iter {
+        if webbrowser::open(url).is_err() {
+            println!("'{}' is not url", url);
+        }
+    }
 }
