84dcb81233dd93a44896312d0ca4280f5b681baf
mk3250d07295y@aol.com <Yoika>
2022-12-25 18:29:43 +0900

    add database.rsの作成

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
 use sqlx::sqlite::{SqlitePool};
-use webbrowser;
 use structopt::StructOpt;
+
+pub(crate) mod database;
 
 #[derive(StructOpt)]
 struct Args {
@@ -24,101 +25,30 @@ async fn main() -> anyhow::Result<()> {
 
     match args.cmd {
         Some(Command::List) => {
-            lists(&pool).await?;
+            database::lists(&pool).await?;
         }
         Some(Command::Add { name, url}) => {
-            let _ = add(&pool, &name, url).await;
+            database::add(&pool, &name, url).await?;
             println!("「{}」 is added!", name);
         }
         Some(Command::Delete { name }) => {
-            let _ = delete(&pool, &name).await;
+            database::delete(&pool, &name).await?;
             println!("「{}」 is deleted!", name);
         }
         Some(Command::Brow { names } ) => {
             let args_iter = names[0..].iter();
             for name in args_iter {
-                let _ = find_by_name(&pool, name).await;
+                database::find_by_name(&pool, name).await?;
             }
         }
         None => {
             let args: Vec<String> = env::args().collect();
             let args_iter = args[1..].iter();
             for name in args_iter {
-                let _ = find_by_name(&pool, name).await;
+                database::find_by_name(&pool, name).await?;
             }
         }
     }
-
-    Ok(())
-}
-
-async fn lists(pool: &SqlitePool) -> anyhow::Result<()> {
-    let recs = sqlx::query!(
-        r#"
-SELECT id, name, url
-FROM sites
-ORDER BY id
-        "#
-    )
-    .fetch_all(pool)
-    .await?;
-
-    for rec in recs {
-        println!(
-            "{} | {} | {}",
-            rec.id,
-            rec.name,
-            rec.url,
-        );
-    }
-
-    Ok(())
-}
-
-async fn find_by_name(pool: &SqlitePool, name: &str) -> anyhow::Result<()> {
-    let rec = sqlx::query!(
-        r#"
-SELECT url FROM sites WHERE name = ?
-        "#,
-        name
-    )
-    .fetch_one(pool)
-    .await?;
-
-    if webbrowser::open(&rec.url).is_err() {
-        println!("'{}' is not url", rec.url);
-    }
-
-    Ok(())
-}
-
-async fn add(pool: &SqlitePool, name: &str, url: String) -> anyhow::Result<i64> {
-    let mut conn = pool.acquire().await?;
-    let id = sqlx::query!(
-        r#"
-INSERT INTO sites (name, url) VALUES(?, ?)
-        "#,
-        name,
-        url
-    )
-    .execute(&mut conn)
-    .await?
-    .last_insert_rowid();
-
-    Ok(id)
-}
-
-async fn delete(pool: &SqlitePool, name: &str) -> anyhow::Result<()> {
-    let mut conn = pool.acquire().await?;
-    let _ = sqlx::query!(
-        r#"
-DELETE FROM sites WHERE name = ?
-        "#,
-        name,
-    )
-    .execute(&mut conn)
-    .await?
-    .last_insert_rowid();
 
     Ok(())
 }
